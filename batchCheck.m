720886a0668dd446122ba4e58f133f8fe7cb5e9a
Geoffrey Jones <jonesg5@rpi.edu>
2013-09-09 17:26:29 -0400

    Added program to check files

---
+++ batchCheck.m
@@ -0,0 +1,46 @@
+function batchCheck
+%BATCHPROCESS Summary of this function goes here
+%   Detailed explanation goes here
+startDir = 'C:\Users\jonesg5\Desktop\NIDA';
+% actiDir = uigetdir(startDir,'Select Actiwatch folder.');
+actiDir = fullfile(startDir,'actiwatchData');
+% dimeDir = uigetdir(startDir,'Select Dimesimeter folder.');
+dimeDir = fullfile(startDir,'dimesimeterData');
+% [sleepFile,sleepDir,~] = uigetfile([startDir,filesep,'.xlsx'],'Select Sleep Log');
+sleepDir = startDir;
+sleepFile = 'sleepLog.xlsx';
+
+[logSubject,bedTime,wakeTime] = importSleepLog(fullfile(sleepDir,sleepFile));
+
+actiListing = dir(fullfile(actiDir,'sub*.csv'));
+
+% Preallocate
+n = length(actiListing);
+
+figure;
+for i1 = 1:n
+    % select actiwatch file
+    actiFile = fullfile(actiDir,actiListing(i1).name);
+    
+    % select dimesimeter file
+    fileBase = regexprep(actiListing(i1).name,'\.csv','','ignorecase');
+    dimeFile = fullfile(dimeDir,[fileBase,'.txt']);
+    
+    % extract subject from filename
+    [~,name,~] = fileparts(actiFile);
+    pattern    = 'sub(\d*)_(\d*)-(\d*)-(\d*)_(\d*)-(\d*)-(\d*)';
+    tokens     = regexpi(name,pattern,'tokens');
+    numTokens  = str2double(tokens{1});
+    subject    = numTokens(1);
+    
+    % select sleep times
+    idx = logSubject == subject;
+    
+    % check files
+    checkFiles(actiFile,dimeFile,bedTime(idx),wakeTime(idx));
+    
+end
+close('all');
+
+end
+
---
+++ checkFiles.m
@@ -0,0 +1,124 @@
+function checkFiles(actiFile,dimeFile,bedTimes,wakeTimes)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+saveDir = 'C:\Users\jonesg5\Desktop\NIDA\checkFigs';
+[aTime,aActivity,subject] = importActiwatch(actiFile);
+
+% check if daysimeter file exists and import
+if exist(dimeFile,'file') == 2
+    [~,dTime,~,~,dCS,dActivity] = importDimesimeter(dimeFile);
+else
+    dTime = aTime;
+    an = length(aTime);
+    dCS = -1*ones(an,1);
+    dActivity = -1*ones(an,1);
+end
+
+% determine trial
+if weekday(aTime(1)) == 2
+    trial = 'week';
+elseif weekday(aTime(1)) == 6
+    trial = 'weekend';
+else
+    trial = 'error';
+end
+
+% determine number of days (noon to noon)
+tempStart = ceil(aTime(1));
+if tempStart - aTime(1) > .5
+    startTime = tempStart - .5;
+else
+    startTime = tempStart + .5;
+end
+tempStop = floor(aTime(end));
+if aTime(end) - tempStop > .5
+    stopTime = tempStop + .5;
+else
+    stopTime = tempStop - .5;
+end
+
+days = ceil(stopTime - startTime);
+
+% apply gaussian filter to data
+aEpoch = ((aTime(2)-aTime(1))*(24*3600)); % sample epoch in seconds
+aWin = floor(300/aEpoch); % number of samples in 5 minutes
+dEpoch = ((dTime(2)-dTime(1))*(24*3600)); % sample epoch in seconds
+dWin = floor(300/dEpoch); % number of samples in 5 minutes
+aActivity = gaussian(aActivity, aWin);
+dActivity = gaussian(dActivity, dWin);
+dCS = gaussian(dCS, dWin);
+
+% plot files
+color1 = [245,135,35]/255; % orange
+color2 = [188,188,188]/255; % grey
+color3 = [27,50,95]/255; % blue
+color4 = [148,186,101]/255; % green
+color5 = [232,55,62]/255; % red
+lineWidth = 1.5;
+
+yMin = -0.1;
+yMax = 0.8;
+
+for i1 = 1:days
+    startDay = startTime + i1 - 1;
+    stopDay = startTime + i1;
+    aidx = aTime >= startDay & aTime <= stopDay;
+    didx = dTime >= startDay & dTime <= stopDay;
+    ati = (aTime(aidx) - floor(startDay))*24;
+    dti = (dTime(didx) - floor(startDay))*24;
+    a = axes('XTick',12:1:36,'XLim',[12 36],...
+        'YTick',yMin:.1:yMax,'YLim',[yMin,yMax]);
+    hold(a);
+    
+    plot(a,ati,aActivity(aidx)/max(aActivity),...
+        'Color',color1,'LineWidth',lineWidth,...
+        'DisplayName','Actiwatch AI (normalized)');
+    
+    plot(a,dti,dCS(didx),...
+        'Color',color2,'LineWidth',lineWidth,...
+        'DisplayName','CS');
+    
+    plot(a,dti,dActivity(didx),...
+        'Color',color3,'LineWidth',lineWidth,...
+        'DisplayName','Daysimeter AI');
+    % plot bed times
+    bidx = bedTimes >= startDay & bedTimes <= stopDay & ~isnan(bedTimes);
+    dayBed = (bedTimes(bidx) - floor(startDay))*24;
+    bn = length(dayBed);
+    if bn >= 1
+        for i2 = 1:bn
+            bedLine = plot(a,[dayBed(i2),dayBed(i2)],[yMin,yMax],...
+                'Color',color4,'LineWidth',lineWidth);
+            set(get(get(bedLine,'Annotation'),'LegendInformation'),...
+                'IconDisplayStyle','off'); % Exclude line from legend
+            text(dayBed(i2)+.01,-.05,datestr(dayBed(i2),'HH:MM'));
+        end
+    end
+    % plot wake times
+    widx = wakeTimes >= startDay & wakeTimes <= stopDay & ~isnan(wakeTimes);
+    dayWake = (wakeTimes(widx) - floor(startDay))*24;
+    wn = length(dayWake);
+    if wn >= 1
+        for i3 = 1:wn
+            wakeLine = plot(a,[dayWake(i3),dayWake(i3)],[yMin,yMax],...
+                'Color',color5,'LineWidth',lineWidth);
+            set(get(get(wakeLine,'Annotation'),'LegendInformation'),...
+                'IconDisplayStyle','off'); % Exclude line from legend
+            text(dayWake(i3)+.01,-.05,datestr(dayWake(i3),'HH:MM'));
+        end
+    end
+
+%     datetick(a,'x','HH','keepticks','keeplimits');
+    title(a,{['Subject ',num2str(subject),' ',trial];...
+        [datestr(min(aTime(aidx)),'mm/dd/yyyy'),' - ',datestr(max(aTime(aidx)),'mm/dd/yyyy')]})
+    legend(a,'show','Location','NorthOutside','Orientation','horizontal');
+    fileName = ['sub',num2str(subject,'%02.0f'),...
+        '_',datestr(startDay,'yyyy-mm-dd'),...
+        '_',datestr(stopDay,'yyyy-mm-dd')];
+    saveas(gcf,fullfile(saveDir,[fileName,'.fig']));
+    saveas(gcf,fullfile(saveDir,[fileName,'.pdf']));
+    clf;
+end
+
+end
+

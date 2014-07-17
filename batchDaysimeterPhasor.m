4ac38e849daf3d1c776d028a870dc770d7864c29
Geoffrey Jones <jonesg5@rpi.edu>
2014-07-17 17:17:56 -0400

    Updated batchDaysimeterPhasor to combine files

--- batchDaysimeterPhasor.m
+++ batchDaysimeterPhasor.m
@@ -2,63 +2,126 @@ function batchDaysimeterPhasor
 %BATCHPROCESSPHASOR Summary of this function goes here
 %   Detailed explanation goes here
 
-addpath('phasorAnalysis');
-startDir = '\\ROOT\projects\NIDA';
-dimeDir = fullfile(startDir,'daysimeterDataLocalTime');
-dimeListing = dir(fullfile(dimeDir,'sub*.txt'));
+[parentDir,~,~] = fileparts(pwd);
+phasorToolkit = fullfile(parentDir,'PhasorAnalysis');
+addpath(phasorToolkit);
 
-% Preallocate output dataset
-n = length(dimeListing);
-out = struct;
-out.subject = cell(n,1);
-out.trial = cell(n,1);
-out.phasorMagnitude = cell(n,1);
-out.phasorAngle = cell(n,1);
-out.IS = cell(n,1);
-out.IV = cell(n,1);
-out.mCS = cell(n,1);
-out.MagH = cell(n,1);
-out.f24abs = cell(n,1);
-out.meanDaytimeCS = cell(n,1);
-out.peakCSperDay = cell(n,1);
-out.centroidHour = cell(n,1);
+projectDir = fullfile([filesep,filesep],'root','projects','NIDA','2011');
+inputDir = fullfile(projectDir,'daysimeterDataLocalTime');
+FileListing = dir(fullfile(inputDir,'sub*.txt'));
 
-for i1 = 1:n
+% Extract information from filename
+%   Prepare listing of file names for decomposition
+cellFileListing = struct2cell(FileListing);
+fileNameArray = cellFileListing(1,:)';
+%   Decompose file names
+pattern = 'sub(\d*)_(\d*)-(\d*)-(\d*)_(\d*)-(\d*)-(\d*).txt';
+tokens = regexpi(fileNameArray,pattern,'tokens');
+%	Flatten nested parts of file names and convert from strings to numbers
+tempTokens1 = cat(1,tokens{:,:});
+tempTokens2 = cat(1,tempTokens1{:,:});
+numTokens = str2double(tempTokens2);
+%   Find subjects
+fileSubjectArray = numTokens(:,1);
+unqSubjectArray = unique(fileSubjectArray);
+%   Find date ranges and convert to datenum
+fileStartArray = datenum(numTokens(:,2),numTokens(:,3),numTokens(:,4));
+fileEndArray = datenum(numTokens(:,5),numTokens(:,6),numTokens(:,7));
+
+% Preallocate output datasets
+nSubjects = numel(unqSubjectArray);
+emptyCell = cell(nSubjects,1);
+Phasor = dataset;
+Phasor.subject = unqSubjectArray;
+Phasor.wkDayMagnitude = emptyCell;
+Phasor.wkDayAngleHrs = emptyCell;
+Phasor.wkEndMagnitude = emptyCell;
+Phasor.wkEndAngleHrs = emptyCell;
+Phasor.combinedMagnitude = emptyCell;
+Phasor.combinedAngleHrs = emptyCell;
+
+for i1 = 1:nSubjects
+    % Find files that match the subject number
+    idxSubject = fileSubjectArray == unqSubjectArray(i1);
+    dimeFile = fullfile(inputDir,fileNameArray(idxSubject));
+    
+    subjectStartArray = fileStartArray(idxSubject);
+    subjectEndArray = fileEndArray(idxSubject);
+    
+    % Preallocate variables
+    nFiles = numel(dimeFile);
+    timeArray = cell(nFiles,1);
+    csArray = cell(nFiles,1);
+    activityArray = cell(nFiles,1);
+    
+    for i2 = 1:nFiles
     % import dimesimeter file
-    dimeFile = fullfile(dimeDir,dimeListing(i1).name);
-    [~,time1,~,~,CS1,activity1] = importDimesimeter(dimeFile);
-    % Extract information from filename
-    [~,name,~] = fileparts(dimeListing(i1).name);
-    pattern = 'sub(\d*)_(\d*)-(\d*)-(\d*)_(\d*)-(\d*)-(\d*)';
-    tokens = regexpi(name,pattern,'tokens');
-    numTokens = str2double(tokens{1});
-    out.subject{i1} = numTokens(1);
-    fileStart = datenum(numTokens(2),numTokens(3),numTokens(4));
-    fileEnd = datenum(numTokens(5),numTokens(6),numTokens(7));
-    % Trim the data
-    idx1 = time1 >= fileStart & time1 < fileEnd + 1;
-    time = time1(idx1);
-    activity = activity1(idx1);
-    CS = CS1(idx1);
-    % analyze the data
-    [out.phasorMagnitude{i1},out.phasorAngle{i1},out.IS{i1},out.IV{i1},...
-        out.mCS{i1},out.MagH{i1},out.f24abs{i1}] = ...
-        phasorAnalysis(time, CS, activity);
-    out.meanDaytimeCS{i1} = daytimeCS(time,CS,39.2833,-76.6167,-5);
-    out.peakCSperDay{i1} = hoursPeakCS(time,CS,.63);
-    [out.centroidHour{i1}, ~] = centroidCS(time,CS);
-    % determine trial
-    if weekday(time(1)) == 2
-        out.trial{i1} = 'week';
-    elseif weekday(time(1)) == 6
-        out.trial{i1} = 'weekend';
-    else
-        out.trial{i1} = 'error';
-    end
+        [~,timeArray{i2,1},~,~,csArray{i2,1},activityArray{i2,1}] = importDimesimeter(dimeFile{i2});
+
+        % Trim the data
+        idx1 = timeArray{i2,1} >= subjectStartArray(i2) & timeArray{i2,1} < subjectEndArray(i2) + 1;
+        timeArray{i2,1} = timeArray{i2,1}(idx1);
+        activityArray{i2,1} = activityArray{i2,1}(idx1);
+        csArray{i2,1} = csArray{i2,1}(idx1);
+        % analyze the data
+        TempPhasor = phasoranalysis(timeArray{i2,1},csArray{i2,1},activityArray{i2,1});
+        
+        % Determine if weekend or weekday trial
+        trialName = trial(timeArray{i2,1});
+        
+        % Reassign output
+        switch trialName
+            case 'weekday'
+                Phasor.wkDayMagnitude{i1} = TempPhasor.magnitude;
+                Phasor.wkDayAngleHrs{i1} = TempPhasor.angleHrs;
+            case 'weekend'
+                Phasor.wkEndMagnitude{i1} = TempPhasor.magnitude;
+                Phasor.wkEndAngleHrs{i1} = TempPhasor.angleHrs;
+            otherwise
+                error('Unknown trial session');
+        end % end of switch
+    end % end of i2
+    
+    % Combine weekday and weekend
+    if nFiles == 2
+        % Combine data
+        timeArray = cat(1,timeArray{:,:});
+        csArray = cat(1,csArray{:,:});
+        activityArray = cat(1,activityArray{:,:});
+        % Sort data by time
+        [timeArray,srtIdx] = sort(timeArray);
+        csArray = csArray(srtIdx);
+        activityArray = activityArray(srtIdx);
+        % Analyze data
+        TempPhasor = phasoranalysis(timeArray,csArray,activityArray);
+        % Reassign output
+        Phasor.combinedMagnitude{i1} = TempPhasor.magnitude;
+        Phasor.combinedAngleHrs{i1} = TempPhasor.angleHrs;
+    end % end of if
+    
+
+end % end of i1
+
+% Average the results
+MeanPhasor = dataset;
+MeanPhasor.wkDayMagnitude = mean(cell2mat(Phasor.wkDayMagnitude));
+MeanPhasor.wkDayAngleHrs = mean(cell2mat(Phasor.wkDayAngleHrs));
+MeanPhasor.wkEndMagnitude = mean(cell2mat(Phasor.wkEndMagnitude));
+MeanPhasor.wkEndAngleHrs = mean(cell2mat(Phasor.wkDayAngleHrs));
+MeanPhasor.combinedMagnitude = mean(cell2mat(Phasor.combinedMagnitude));
+MeanPhasor.combinedAngleHrs = mean(cell2mat(Phasor.combinedAngleHrs));
+
 end
 
-save('daysimeterPhasorAnalysis.mat','out');
-close all;
-
+% determine trial
+function trialName = trial(timeArray)
+dayOfWeek = weekday(timeArray(1));
+switch dayOfWeek
+    case 2
+        trialName = 'weekday';
+    case 6
+        trialName = 'weekend';
+    otherwise
+        trialName = 'error';
 end
-
+end
